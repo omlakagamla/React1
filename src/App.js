617ff0422afc279034a975daeeaed9e1a7cacdfd
Omkar Karande <okarande55@gmail.com>
2022-01-05 14:34:38 +0530

    hii

--- src/App.js
+++ src/App.js
@@ -12,14 +12,3 @@
  * 1. First Two Letter Css Property are now Converted into camelCase Property
  * 2. The value of the property must be in "Double Quote"
  */
-
-export default function App() {
-  return (
-    <div className="container-fluid">
-      <div className="row bg-dark text-light p-3 sticky-top">
-        <div className="col h3"> React </div>
-      </div>
-
-    </div >
-  );
-}
